ef31f40026e0cc738306b4c76e31e515819c7cf3
tycrimm <tycrimm@users.noreply.github.com>
2023-06-26 09:30:33 -0700

    Inline CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  build: {
+    inlineStylesheets: "always",
+  },
 });
